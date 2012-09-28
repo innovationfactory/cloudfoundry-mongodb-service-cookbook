b8185e4bca03254085c23d74e1eba3a339132c11
Ruben Koster <ruben.koster@innovationfactory.eu>
2012-09-28 15:18:57 +0200

    Changed cloudfoundry dependency to cloudfoundry-common

--- metadata.rb
+++ metadata.rb
@@ -9,6 +9,6 @@ version          "0.99.0"
   supports os
 end
 
-%w( cloudfoundry cloudfoundry_service mongodb ).each do |cb|
+%w( cloudfoundry-common cloudfoundry_service mongodb ).each do |cb|
   depends cb
 end
